3fdc00c2d15ceada82cb25af0bdac0c920540c62
KamauDev-maker <oscarnjenga@gmail.com>
2022-03-21 09:22:09 +0300

    declaring variables

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     $("#order-details").hide();
     $("#deliver").hide();
+    let totalPriceArray = [];
+  function Order(size, crust, toppings, amount) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.pizzaPrice = 0;
+    this.amount = amount;
+  }
 });
